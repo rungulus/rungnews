38c1ba39b08d813bc7de9c3957b6bea0a1a3b764
RUNGUSZONE <rungus@rungus.zone>
2023-06-19 00:21:34 -0600

    minor changes, we update live now though.

--- internal/parse-rss-and-print.js
+++ internal/parse-rss-and-print.js
@@ -1,16 +1,15 @@
-let parser = new RSSParser();
-parser.parseURL('npr-061423.xml', function(err, feed) { 
-  //this needs to be local, or else we get a CORS error (Access-Control-Allow-Origin)
-  //i'm pretty sure its not github, maybe it's cloudflare, but it also does happen locally
-  //so im going to hedge a bet that its the rss-parser.min.js library
-  //todo: #4 grab current rss feeds without running into CORS issues
+function parseRSS(link){
+  let parser = new RSSParser();
+  parser.parseURL(link, function(err, feed) { 
   if (err) throw err;
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 11; i++) {
     const entry = feed.items[i];
     console.log('Entry:', entry); // for debugging
     printPost(entry, i);
   }
 });
+}
+
 function extractImageUrl(entry) {
     if (entry.content && entry.content.encoded) {
       const htmlParser = new DOMParser().parseFromString(entry.content.encoded, 'text/html');
@@ -39,26 +38,35 @@ function extractImageUrl(entry) {
     } else {
       console.log('Article Title Element not found.'); // for debugging
     }
-  
+    if (number == 0){ //only article0 has a quote element
+    const articleQuote = document.getElementById('article' + number + 'ArticleQuote');
+    console.log('Article Quote:', articleQuote);
+    articleQuote.innerText = entry.creator;
+    }
+
     const articleImage = document.getElementById('article' + number + 'Image');
     console.log('Article Image Element:', articleImage); // for debugging
+
+    // this doesn't seem to work, and it seems each news site displays these differently, so thats cool!
+    // i wonder if we can rig up a google search api type deal to get a generic image so we don't have to rely on the providers
+    // however, that could be rather odd if the images don't make sense
+    // todo: figure out what we are going to do for images!
+    //
+    // if (articleImage) {
+    //   const imageUrl = extractImageUrl(entry);
+    //   console.log('Image URL:', imageUrl); // for debugging
   
-    if (articleImage) {
-      const imageUrl = extractImageUrl(entry);
-      console.log('Image URL:', imageUrl); // for debugging
-  
-      if (imageUrl) {
-        articleImage.src = imageUrl;
-        articleImage.className = 'img-fluid';
-      }
-    } else {
-      console.log('Article Image Element not found.'); // for debugging
-    }
+    //   if (imageUrl) {
+    //     articleImage.src = imageUrl;
+    //     articleImage.className = 'img-fluid';
+    //   }
+    // } else {
+    //   console.log('Article Image Element not found.'); // for debugging
+    // }
   }
-  
-  
 
-
-
+parseRSS('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml');
   
-  
+// cnn - http://rss.cnn.com/rss/cnn_topstories.rss <- Access-Control-Allow-Origin
+// nyt - https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml <- works
+// npr - https://feeds.npr.org/1001/rss.xml <- Access-Control-Allow-Origin
